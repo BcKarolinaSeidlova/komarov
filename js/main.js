6c12fa87164d7578b67431cf9bb6fdccdd73cb47
karolinaseidlova@gmail.com <35819148+BcKarolinaSeidlova@users.noreply.github.com>
2018-05-16 21:47:56 +0200

    Add clickable cards and new html files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+let karty = document.getElementsByClassName('karta');
+let linky = document.getElementsByClassName('link');
+
+for (let i = 0; i<karty.length;i++) {
+let karta = karty[i];
+let link = linky[i];
+karta.addEventListener('click', function(){
+
+	link.click()
+});
+
+}
+
+
+/*let karta1 = document.getElementById("karta1");
+let link1 = document.getElementById("z1");
+
+
+	karta1.addEventListener('click', function(){});
+	*/
+
+
+
 function hamburger(x) {
     x.classList.toggle("change");
     let y = document.getElementById('navigation');
@@ -6,3 +29,7 @@ function hamburger(x) {
     } else {
         y.style.display = "flex";
     }
+
+
+
+};
