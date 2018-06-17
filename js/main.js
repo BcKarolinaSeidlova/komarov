bd377e691881d7edc5584c4e604abf789239ee54
karolinaseidlova@gmail.com <35819148+BcKarolinaSeidlova@users.noreply.github.com>
2018-06-18 00:27:09 +0200

    Responsivita + uprava js navigace

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 let karty = document.getElementsByClassName('karta');
 let linky = document.getElementsByClassName('link');
+let navItems = document.getElementsByClassName('nav-item');
+let navLink = document.getElementsByClassName('nav-link');
+let navItemsHis = document.getElementsByClassName('nav-item-his');
+let navLinkHis = document.getElementsByClassName('link-his');
+let ham = document.getElementsByClassName('hamburger')[0];
+let nav =document.getElementsByClassName('navigation')[0];
+
+
+window.addEventListener('resize', function () {
+        if(window.innerWidth >= 900) {
+           nav.style.display = "flex";
+        }
+
+        else {
+            if(ham.classList == 'hamburger change') 
+                {ham.classList.toggle('change'); nav.style.display = "none"; }
+            else {nav.style.display = "none"; }
+    }
+
+});
+
+
 
 for (let i = 0; i<karty.length;i++) {
 let karta = karty[i];
@@ -11,20 +33,43 @@ karta.addEventListener('click', function(){
 
 }
 
+for (let i = 0; i<navItems.length; i++) {
+    let navItem = navItems[i];
+let link = navLink[i];
+navItem.addEventListener('click', function(){
+
+    link.click()
+});
+
+}
+
+for (let i = 0; i<navItemsHis.length; i++) {
+    let navItemHis = navItemsHis[i];
+let linkHis = navLinkHis[i];
+navItemHis.addEventListener('click', function(){
+
+    linkHis.click()
+});
+
+}
 
 
-function hamburger(x) {
-    x.classList.toggle("change");
+
+
+ham.addEventListener('click', hamburger);
+
+function hamburger() {
+    ham.classList.toggle("change");
     let y = document.getElementById('navigation');
+
     if (y.style.display === "flex") {
         y.style.display = "none";
     } else {
         y.style.display = "flex";
     }
 
-
-
 };
+
 
 /*
 * SLIDESHOW
@@ -50,3 +95,5 @@ function showDivs(n) {
     }
     x[slideIndex-1].style.display = "flex"; 
 }
+
+
